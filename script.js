fc1d23364d9bfd7525a99f91ce3ee8f2dc0e60cb
Jugraj99 <97783270+Jugraj99@users.noreply.github.com>
2025-12-19 12:55:05 +0000

    Update and fixes with original hamburger menu and correct live demo links with repositiories.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const toggleButton = document.getElementById("dark-mode-toggle");
-
-toggleButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
